a8fdd8b539904c29ceb5346c546d3c90a706c574
Juan Bonilla <juan.bonilla@segurosbolivar.com>
2025-07-11 09:02:53 -0500

    Se crea el repositorio

---
+++ main.py
@@ -0,0 +1,72 @@
+import sys 
+sys.path.append('.')
+
+#* Librerías para el API
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
+import os
+
+#* Librerías propias para el funcionamiento del API
+from utils.request_postgres import consulta_clientes_aliados
+
+# Parametros básicos y clases
+app = FastAPI() 
+puerto = os.environ.get("PORT", 8080)
+
+# Configuración de CORS
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#* Definición de un endpoint
+descripcion_path = 'API que retorna información de clientes aliados desde BigQuery, consultando por filtros como código agente, documento, producto, etc.'
+summary_path = 'Consulta clientes desde BigQuery con múltiples filtros opcionales (al menos uno requerido)'
+endpoint_end = '/api_consulta_portal_aliados'
+
+# Mock de ejemplo de entrada en Body de request
+"""
+{
+  "codigo_agente": "55903",
+  "tipo_documento": "CC",
+  "id_documento": "1020948732",
+  "nombre": "Luz Elena Arevalo Arenas",
+  "estado_poliza": "Vigente",
+  "producto": "Educadores Plus"
+}
+"""
+
+@app.post(endpoint_end, summary=summary_path, description=descripcion_path)
+async def api_consulta_afiliacion_empresa(body: dict):
+    codigo_agente = body.get("codigo_agente")
+    tipo_documento = body.get("tipo_documento")
+    id_documento = body.get("id_documento")
+    nombre = body.get("nombre")
+    estado_poliza = body.get("estado_poliza")
+    producto = body.get("producto")
+
+    if not any([codigo_agente, tipo_documento, id_documento, nombre, estado_poliza, producto]):
+        return {"error": "Debe proporcionar al menos un parámetro"}
+
+    response = consulta_clientes_aliados(
+        CLAVE_AGENTE=codigo_agente,
+        TIPO_DOCUMENTO_ASEGURADO=tipo_documento,
+        NUMERO_DOCUMENTO_ASEGURADO=id_documento,
+        NOMBRE=nombre,
+        ESTADO_POLIZA=estado_poliza,
+        CODIGO_PRODUCTO=producto
+    )
+    return response
+
+@app.get("/", response_class=RedirectResponse)
+async def redirect_to_docs():
+    return "/docs"
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=int(puerto))
---
+++ utils/request_postgres.py
@@ -0,0 +1,109 @@
+import json
+import sqlalchemy
+import pandas as pd
+import datetime as dt
+
+from utils.connect_sql import getEngine
+
+def request_postgres(input_query, params):
+
+  # Crear el motor de SQLAlchemy
+  engine = getEngine()
+  
+  with engine.connect() as connection:
+
+    results = connection.execute(sqlalchemy.text(input_query), params)
+
+    data = pd.DataFrame(results.fetchall())
+    
+    return data
+
+def consulta_clientes_aliados(CLAVE_AGENTE = None,TIPO_DOCUMENTO_ASEGURADO = None,NUMERO_DOCUMENTO_ASEGURADO = None,NOMBRE = None,ESTADO_POLIZA = None,CODIGO_PRODUCTO = None,):
+    try:
+        filtros = {
+            "CLAVE_AGENTE": CLAVE_AGENTE,
+            "TIPO_DOCUMENTO_ASEGURADO": TIPO_DOCUMENTO_ASEGURADO,
+            "NUMERO_DOCUMENTO_ASEGURADO": NUMERO_DOCUMENTO_ASEGURADO,
+            "NOMBRE": NOMBRE,
+            "ESTADO_POLIZA": ESTADO_POLIZA,
+            "CODIGO_PRODUCTO": CODIGO_PRODUCTO
+        }
+
+        # Inicializar las cláusulas WHERE y los parámetros
+        where_clauses = []
+        params = {}
+
+        # Construir las cláusulas WHERE dinámicamente (solo si el filtro es enviado dentro del body)
+        for key, value in filtros.items():
+            if value is not None:
+                where_clauses.append(f'"{key}" = :{key}')
+                params[key] = value
+
+        # Verificar si se proporcionaron filtros
+        if not where_clauses:
+            return {
+                "error": "Debe proporcionar al menos un filtro"
+            }
+        
+        # Unir las cláusulas WHERE con AND
+        where_sql = " AND ".join(where_clauses)
+
+        # Construir la consulta SQL
+        #TODO: Crar clientes_portal_aliados en api_backend
+        query = f"""
+            SELECT 
+              "CLAVE_AGENTE",
+              "TIPO_DOCUMENTO_ASEGURADO",
+              "NUMERO_DOCUMENTO_ASEGURADO",
+              "NOMBRE",
+              "ESTADO_POLIZA",
+              "CODIGO_PRODUCTO",
+              "CIUDAD",
+              "DIRECCION",
+              "CELULAR",
+              "CORREO",
+              "PRODUCTOS_VIGENTES",
+              "FECHA_NACIMIENTO",
+              "EDAD",
+              "ESTADO_CIVIL",
+              "FECHA_SARLAFT_SIMPLIFICADO",
+              "FECHA_SARLAFT_ORDINARIO",
+              "NUMERO_POLIZA",
+              "PRODUCTO_POLIZA",
+              "ESTADO_POLIZA",
+              "ROL",
+              "VALOR_ASEGURADO",
+              "VALOR_PRIMA",
+              "VALOR_ASISTENCIA",
+              "VIGENCIA_INICIO_POLIZA",
+              "VIGENCIA_FIN_POLIZA",
+              "FECHA_EMISION",
+              "CODIGO_RAMO_EMISION",
+              "NOMBRE_RAMO_EMISION",
+              "CODIGO_SUBPRODUCTO",
+              "NOMBRE_SUBPRODUCTO",
+              "NUMERO_POLIZA_MADRE",
+              "CANTIDAD_RIESGOS",
+              "TIPO_DOCUMENTO_TOMADOR",
+              "NUMERO_DOCUMENTO_TOMADOR",
+              "NOMBRE_TOMADOR",
+              "VIGENCIA_INICIO_POLIZA_MADRE",
+              "VIGENCIA_FIN_POLIZA_MADRE",
+              "PERIODICIDAD_PAGO",
+              "FORMA_PAGO",
+              "CANAL_DE_DESCUENTO",
+              "VALOR_ASEGURADO"
+            FROM "api_backend"."t_cliente_portal_aliados"
+            WHERE {where_sql};
+        """
+
+        response = request_postgres(query, params)
+        response = json.loads(response.to_json(orient='records', date_format='iso'))
+
+        return {"clientes": response}
+
+    except Exception as e:
+        return {
+            "error": str(e),
+            "message": "Excepción en la consulta de clientes aliados"
+        }

5db5bad9f9ede4b896d9cabde0758118e4173504
Juan Bonilla <juan.bonilla@segurosbolivar.com>
2025-07-11 13:25:38 -0500

    cambio en consulta comillas

--- utils/request_postgres.py
+++ utils/request_postgres.py
@@ -36,7 +36,10 @@ def consulta_clientes_aliados(CLAVE_AGENTE = None,TIPO_DOCUMENTO_ASEGURADO = Non
         # Construir las cláusulas WHERE dinámicamente (solo si el filtro es enviado dentro del body)
         for key, value in filtros.items():
             if value is not None:
-                where_clauses.append(f"{key} = '{value}'")
+                if key in ("CLAVE_AGENTE","NUMERO_DOCUMENTO_ASEGURADO"):
+                    where_clauses.append(f"{key} = {value}")
+                else:
+                    where_clauses.append(f"{key} = '{value}'")
                 params[key] = value
 
         # Verificar si se proporcionaron filtros
@@ -48,51 +51,52 @@ def consulta_clientes_aliados(CLAVE_AGENTE = None,TIPO_DOCUMENTO_ASEGURADO = Non
         # Unir las cláusulas WHERE con AND
         where_sql = " AND ".join(where_clauses)
 
+        params = {}
         # Construir la consulta SQL
         query = f"""
             SELECT 
-              CLAVE_AGENTE,
-              TIPO_DOCUMENTO_ASEGURADO,
-              NUMERO_DOCUMENTO_ASEGURADO,
-              NOMBRE,
-              ESTADO_POLIZA,
-              CODIGO_PRODUCTO,
-              CIUDAD,
-              DIRECCION,
-              CELULAR,
-              CORREO,
-              PRODUCTOS_VIGENTES,
-              FECHA_NACIMIENTO,
-              EDAD,
-              ESTADO_CIVIL,
-              FECHA_SARLAFT_SIMPLIFICADO,
-              FECHA_SARLAFT_ORDINARIO,
-              NUMERO_POLIZA,
-              PRODUCTO_POLIZA,
-              ESTADO_POLIZA,
-              ROL,
-              VALOR_ASEGURADO,
-              VALOR_PRIMA,
-              VALOR_ASISTENCIA,
-              VIGENCIA_INICIO_POLIZA,
-              VIGENCIA_FIN_POLIZA,
-              FECHA_EMISION,
-              CODIGO_RAMO_EMISION,
-              NOMBRE_RAMO_EMISION,
-              CODIGO_SUBPRODUCTO,
-              NOMBRE_SUBPRODUCTO,
-              NUMERO_POLIZA_MADRE,
-              CANTIDAD_RIESGOS,
-              TIPO_DOCUMENTO_TOMADOR,
-              NUMERO_DOCUMENTO_TOMADOR,
-              NOMBRE_TOMADOR,
-              VIGENCIA_INICIO_POLIZA_MADRE,
-              VIGENCIA_FIN_POLIZA_MADRE,
-              PERIODICIDAD_PAGO,
-              FORMA_PAGO,
-              CANAL_DE_DESCUENTO,
-              VALOR_ASEGURADO
-            FROM api_backend.paliados_clientes
+              "CLAVE_AGENTE",
+              "TIPO_DOCUMENTO_ASEGURADO",
+              "NUMERO_DOCUMENTO_ASEGURADO",
+              "NOMBRE",
+              "ESTADO_POLIZA",
+              "CODIGO_PRODUCTO",
+              "CIUDAD",
+              "DIRECCION",
+              "CELULAR",
+              "CORREO",
+              "PRODUCTOS_VIGENTES",
+              "FECHA_NACIMIENTO",
+              "EDAD",
+              "ESTADO_CIVIL",
+              "FECHA_SARLAFT_SIMPLIFICADO",
+              "FECHA_SARLAFT_ORDINARIO",
+              "NUMERO_POLIZA",
+              "PRODUCTO_POLIZA",
+              "ESTADO_POLIZA",
+              "ROL",
+              "VALOR_ASEGURADO",
+              "VALOR_PRIMA",
+              "VALOR_ASISTENCIA",
+              "VIGENCIA_INICIO_POLIZA",
+              "VIGENCIA_FIN_POLIZA",
+              "FECHA_EMISION",
+              "CODIGO_RAMO_EMISION",
+              "NOMBRE_RAMO_EMISION",
+              "CODIGO_SUBPRODUCTO",
+              "NOMBRE_SUBPRODUCTO",
+              "NUMERO_POLIZA_MADRE",
+              "CANTIDAD_RIESGOS",
+              "TIPO_DOCUMENTO_TOMADOR",
+              "NUMERO_DOCUMENTO_TOMADOR",
+              "NOMBRE_TOMADOR",
+              "VIGENCIA_INICIO_POLIZA_MADRE",
+              "VIGENCIA_FIN_POLIZA_MADRE",
+              "PERIODICIDAD_PAGO",
+              "FORMA_PAGO",
+              "CANAL_DE_DESCUENTO",
+              "VALOR_ASEGURADO"
+            FROM "api_backend"."paliados_clientes"
             WHERE {where_sql};
         """
 
